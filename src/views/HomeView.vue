d65694146e366cd6801028981296137d535b4e4c
khidd <kennedynyambura5981@gmail.com>
2023-07-05 10:24:38 +0300

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   home
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    console.log('setup')
+  },
+  created(){
+    console.log('created')
+  },
+  mounted(){
+    console.log('mounted')
   }
 }
 </script>
