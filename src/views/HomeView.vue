f5f20a6b2dda8795cf17f0faf1aaf1b2a9ce1183
khidd <kennedynyambura5981@gmail.com>
2023-07-06 12:30:23 +0300

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="home">
    home
+   <p ref="p">My name is {{ name }}</p>
+   <p>I am {{ age }} years old</p>
+   <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
     console.log('setup')
-  },
-  created(){
-    console.log('created')
-  },
-  mounted(){
-    console.log('mounted')
+
+    const p = ref(null)
+
+    let name = 'Ken'
+    let age = 30
+
+    const handleClick = () => {
+      console.log(p)
+    }
+
+    return {name, age, handleClick, p}
+
   }
 }
 </script>
